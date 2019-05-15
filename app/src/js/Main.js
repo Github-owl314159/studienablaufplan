57739087e9850dbcc9b1af62c2f2d5531e1d174c
sirrollodev <sirrollodev@gmail.com>
2019-05-16 00:42:41 +0200

    testing

--- app/src/js/Main.js
+++ app/src/js/Main.js
@@ -4,8 +4,19 @@ const app = () => {
 		buttonArr[i] = [];
 		for (let j = 0; j < 6; j++) {
 			buttonArr[i][j] = document.getElementById(`module-${i + 1}-${j + 1}`);
+			buttonArr[i][j].addEventListener("click", (event) => {
+				console.log(event);
+			});
 		}
 	}
+
+	// Testumgebung
+	buttonArr[0][0].addEventListener("click", () => {
+		const body = document.getElementsByTagName("body");
+		const testDiv =	document.createElement("div");
+		body[0].appendChild(testDiv);
+	});
+	//  Ende von Testumgebung
 };
 
 app();
